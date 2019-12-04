9bf1760dcd13883ea3cc1844eb0fa5379780c03d
Sumanth Chinthagunta <xmlking@gmail.com>
2019-12-03 19:40:23 -0800

    adding InsecureSkipVerify to make micro-call work with tls backends

--- main.go
+++ main.go
@@ -1,23 +1,33 @@
 package main
 
 import (
+	"crypto/tls"
+	"os"
+
 	"github.com/micro/go-micro/client"
 	"github.com/micro/go-micro/server"
 
-	// "github.com/micro/go-micro/transport"
-	// gtransport "github.com/micro/go-micro/transport/grpc"
 	"github.com/micro/micro/cmd"
 
-	cli "github.com/micro/go-micro/client/grpc"
-	srv "github.com/micro/go-micro/server/grpc"
+	gc "github.com/micro/go-micro/client/grpc"
+	gs "github.com/micro/go-micro/server/grpc"
 )
 
 func main() {
 	// setup client/server to use grpc
-	// client.DefaultClient = cli.NewClient(client.Transport(gtransport.NewTransport(transport.Secure(true))))
-	client.DefaultClient = cli.NewClient()
-	server.DefaultServer = srv.NewServer()
+	if _, found := os.LookupEnv("INSECURE_SKIP_VERIFY"); found {
+		client.DefaultClient = gc.NewClient(
+			gc.AuthTLS(&tls.Config{InsecureSkipVerify: true}),
+		)
+	} else {
+		client.DefaultClient = gc.NewClient()
+	}
 
-	cmd.Init()
+	server.DefaultServer = gs.NewServer()
 
+	cmd.Init(
+	// proxy.WithClient(gc.NewClient(
+	// 	gc.AuthTLS(&tls.Config{InsecureSkipVerify: true}),
+	// )),
+	)
 }
