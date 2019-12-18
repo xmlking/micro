9bd1d7784a6821d9c9e16e014be3e86c77e6ae06
Sumanth Chinthagunta <xmlking@gmail.com>
2019-12-17 19:56:07 -0800

    micro now support CORS by default

--- plugin.go
+++ plugin.go
@@ -4,10 +4,6 @@ import (
 	// Flags usage of k8s plugins `micro --registry=memory --selector=static`
 	_ "github.com/micro/go-plugins/client/selector/static"
 
-	// Flags usage of cors plugin `micro --cors-allowed-headers=X-Custom-Header --cors-allowed-origins=someotherdomain.com  --cors-allowed-methods=POST`
-	"github.com/micro/go-plugins/micro/cors"
-	"github.com/micro/micro/plugin"
-
 	// Flags `micro --broker=googlepubsub`
 	_ "github.com/micro/go-plugins/broker/googlepubsub"
 )
@@ -17,7 +13,4 @@ func init() {
 	// set values for registry/selector
 	// os.Setenv("MICRO_REGISTRY", "kubernetes")
 	// os.Setenv("MICRO_SELECTOR", "static")
-
-	// setup cors plugin
-	_ = plugin.Register(cors.NewPlugin())
 }
